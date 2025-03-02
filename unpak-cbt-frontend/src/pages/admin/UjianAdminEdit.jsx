608858d29bbbd1001c30ebc4a5df9709764aca65
Aprea <apreak@gmail.com>
2025-03-02 15:48:39 +0700

    add file

---
+++ unpak-cbt-frontend/src/pages/admin/UjianAdminEdit.jsx
@@ -0,0 +1,11 @@
+const UjianAdminEdit = () => {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold">Ujian Admin Edit</h1>
+        
+      </div>
+    );
+  };
+  
+  export default UjianAdminEdit;
+  
--- unpak-cbt-frontend/src/pages/admin/UjianAdminTambah.jsx
+++ unpak-cbt-frontend/src/pages/admin/UjianAdminTambah.jsx
@@ -3,8 +3,7 @@
 const UjianAdminTambah = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Dashboard Mahasiswa</h1>
-      <p>Selamat datang! Pilih ujian Anda.</p>
+      <h1 className="text-2xl font-bold">Ujian Admin Tambah</h1>
     </div>
   );
 };
